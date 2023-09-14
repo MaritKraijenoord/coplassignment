935d7f7862e248c3d34b8cc53f44d9306dc5cd3e
A.M. Kraijenoord <s3180131@vuw.leidenuniv.nl>
2023-09-14 10:12:42 +0200

    connecting vscode and creating file

---
+++ main.cc
@@ -0,0 +1,8 @@
+// Main program for Concepts of Programming Languages
+// Assignment 1
+// Marit Kraijenoord, s3180131
+// Jasmijn Makop, s3688321
+// DATE
+
+#include <iostream>
+using namespace std;
